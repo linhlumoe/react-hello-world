05ead06754baf83cd4395fb1346339bc523e9e46
Kylie <khanhlinh970@gmail.com>
2018-03-17 16:12:38 +0700

    state example

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from './styles'
 
-const Header = ({title}) => (
+const Header = ({title, onClick}) => (
   <header style={styles.header}>
     <h1 style={styles.title}>{title}</h1>
+    <button onClick={onClick}>Click me!</button>
   </header>
 );
 
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,13 +3,37 @@ import styles, { red } from './styles.js';
 import Header from '../../components/Header'
 
 class App extends Component {
+  state = {
+    showIntro: true,
+  }
+
+  toogleIntro = () => {
+    const { showIntro } = this.state;
+
+    this.setState({
+      showIntro: !showIntro,
+    });
+  }
+
+  headerClick = () => {
+    alert('Just clicked button in header!')
+  }
+
+
   render() {
+    const { showIntro } = this.state;
     return (
       <div style={styles.container}>
-        <Header title="Welcome to React"/>
-        <p style={styles.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header title="Welcome to React"
+                onClick={this.headerClick}
+        />
+
+        <button onClick={this.toogleIntro}>Toogle Intro</button>
+        {
+          showIntro && <p style={styles.intro}>
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                      </p>
+        }
       </div>
     );
   }
